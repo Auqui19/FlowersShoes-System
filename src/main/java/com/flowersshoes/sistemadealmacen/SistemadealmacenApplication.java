d8d3045241456b120858a8462c8a1652d92d7434
Alonso Auqui <andriod.pvp@gmail.com>
2024-04-21 21:45:18 -0500

    Añadiendo proyecto

---
+++ src/main/java/com/flowersshoes/sistemadealmacen/SistemadealmacenApplication.java
@@ -0,0 +1,13 @@
+package com.flowersshoes.sistemadealmacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadealmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadealmacenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowersshoes/sistemadealmacen/SistemadealmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowersshoes.sistemadealmacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadealmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
