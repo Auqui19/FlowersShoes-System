9d5b6c4b5ae2a2f1ce8dd5f414c9a3878b564de4
Alonso Auqui <andriod.pvp@gmail.com>
2024-04-21 22:08:14 -0500

    Modifico la entidad color

--- src/main/java/com/flowersshoes/sistemadealmacen/model/Color.java
+++ src/main/java/com/flowersshoes/sistemadealmacen/model/Color.java
@@ -1,5 +1,11 @@
 package com.flowersshoes.sistemadealmacen.model;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
 public class Color {
 
+    private String idColor;
 }
