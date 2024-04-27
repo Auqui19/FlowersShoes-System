7da7e0b323a707a6480b5add8d31222bac586647
RenzoFlores <renzofpe@gmail.com>
2024-04-27 00:07:45 -0500

    Creacion entidad Ventas

---
+++ src/main/java/com/flowersshoes/sistemadealmacen/model/Ventas.java
@@ -0,0 +1,37 @@
+package com.flowersshoes.sistemadealmacen.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "tb_ventas")
+public class Ventas {
+    @Id
+    private int idventa;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idtra")
+    private Trabajador trabajador;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idcli")
+    private Cliente cliente;
+
+
+    private Date fecha;
+    private Double total;
+    private String estado;
+    private String estadocomprobante;
+
+
+}
