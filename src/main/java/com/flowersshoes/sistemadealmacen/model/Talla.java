4c3955e0965be5db1c373bb1ffd302cfb1431e7d
joss2110 <g.j.torres.perez@gmail.com>
2024-05-07 23:40:55 -0500

    agreganto todos los archivos funcionales para color y talla

--- src/main/java/com/flowersshoes/sistemadealmacen/model/Talla.java
+++ src/main/java/com/flowersshoes/sistemadealmacen/model/Talla.java
@@ -1,5 +1,6 @@
 package com.flowersshoes.sistemadealmacen.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,7 +21,8 @@ public class Talla {
     private String talla;
     private String estado;
 
-    //@OneToMany(mappedBy = "talla" , cascade = {CascadeType.PERSIST , CascadeType.REMOVE})
-    //private List<Producto> productos;
+    @OneToMany(mappedBy = "talla" , cascade = {CascadeType.PERSIST , CascadeType.REMOVE})
+    @JsonIgnoreProperties("talla")
+    private List<Producto> productos;
 
 }
